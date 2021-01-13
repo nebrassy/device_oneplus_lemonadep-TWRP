3927dc7da46db63e2ce83cc29f4641a016238abd
LuK1337 <priv.luk@gmail.com>
2021-01-13 12:08:27 +0100

    instantnoodlep: Build PowerShare HAL
    Change-Id: Ib4d973baddd71482f2e2535194c5fabc75ca21a0

--- device.mk
+++ device.mk
@@ -48,6 +48,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/odm_feature_list:$(TARGET_COPY_OUT_ODM)/etc/odm_feature_list
 
+# PowerShare
+PRODUCT_PACKAGES += \
+    vendor.lineage.powershare@1.0-service.oneplus_kona
+
 PRODUCT_SHIPPING_API_LEVEL := 29
 
 # Inherit from oneplus sm8250-common
