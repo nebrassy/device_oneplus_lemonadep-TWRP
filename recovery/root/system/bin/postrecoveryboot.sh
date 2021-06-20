5c01963cc6dc089ec7625ddf48d33bc322e193ba
nebrassy <nebras30@gmail.com>
2021-06-20 21:25:27 +0200

    load included kernel modules

--- recovery/root/system/bin/postrecoveryboot.sh
+++ recovery/root/system/bin/postrecoveryboot.sh
@@ -6,8 +6,8 @@ copy-modules()
 {
     if grep -Fq twrpfastboot /proc/cmdline
     then
-        echo "using inline modules (fastboot boot)"
-        exit 0
+        echo "using ramdisk modules (fastboot boot)"
+        return
     fi
 
     # may need e.g. /lib/modules/5.4-gki/ and modules.load vs. modules.load.recovery checked for future-proof OOS as well?
@@ -24,8 +24,9 @@ copy-modules()
     fi
 
     echo "using vendor$suffix modules"
-    mkdir -p /v /vendor/lib/modules
+    mkdir /v
     mount -t ext4 -o ro /dev/block/mapper/vendor$suffix /v
+    rm -f /vendor/lib/modules/*
     cp -afR /v/lib/modules/* /vendor/lib/modules/
     umount /v
     rmdir /v
@@ -34,6 +35,13 @@ copy-modules()
 # hack around touchscreen modules not included in OOS vendor_boot
 install-touch()
 {
+    # may need e.g. /lib/modules/5.4-gki/ and modules.load vs. modules.load.recovery checked for future-proof OOS as well?
+    if [ -f /lib/modules/modules.load.recovery -a -f /lib/modules/touchscreen.ko ] && lsmod | grep -Fq touchscreen
+    then
+        echo "vendor_boot touchscreen modules already loaded"
+        exit 0
+    fi
+
     if [ ! -f /vendor/lib/modules/touchscreen.ko ]
     then
         echo "! vendor touchscreen modules not found"
