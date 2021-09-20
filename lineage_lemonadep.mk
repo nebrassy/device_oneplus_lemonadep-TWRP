fbe524e117e09e704d91324d461858c772a5ffbd
Bruno Martins <bgcngm@gmail.com>
2021-09-20 18:25:39 +0100

    lemonadep: Update blobs from OOS 11.2.9.9.LE15BA
    Change-Id: I82193d92c1ab7cd04bd7996af0926a4e81594139

--- lineage_lemonadep.mk
+++ lineage_lemonadep.mk
@@ -37,4 +37,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=OnePlus9Pro_EEA \
     PRIVATE_BUILD_DESC="OnePlus9Pro-user 11   release-keys"
 
-BUILD_FINGERPRINT := OnePlus/OnePlus9Pro_EEA/OnePlus9Pro:11/RKQ1.201105.002/2107082109:user/release-keys
+BUILD_FINGERPRINT := OnePlus/OnePlus9Pro_EEA/OnePlus9Pro:11/RKQ1.201105.002/2109102008:user/release-keys
