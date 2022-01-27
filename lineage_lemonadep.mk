b9c4bbdd62b831dd1143f07a5247ab26beb30d21
LuK1337 <priv.luk@gmail.com>
2022-01-27 10:53:26 +0100

    lemonadep: Make device name/product prop overrides work
    Change-Id: I64d11103ddb8f9dce70b9bc110745d4aafd667dc

--- lineage_lemonadep.mk
+++ lineage_lemonadep.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021-2022 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -30,11 +30,14 @@ PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := LE2123
 
+PRODUCT_SYSTEM_NAME := OnePlus9Pro_EEA
+PRODUCT_SYSTEM_DEVICE := OnePlus9Pro
+
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE=OnePlus9Pro \
-    PRODUCT_NAME=OnePlus9Pro_EEA \
+    TARGET_DEVICE=OnePlus9Pro \
+    TARGET_PRODUCT=OnePlus9Pro_EEA \
     PRIVATE_BUILD_DESC="OnePlus9Pro-user 11   release-keys"
 
 BUILD_FINGERPRINT := OnePlus/OnePlus9Pro_EEA/OnePlus9Pro:11/RKQ1.201105.002/2111112053:user/release-keys
