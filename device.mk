1c49159babf403d9fd19fb1ae419900390daba1d
Alexander Koskovich <akoskovich@pm.me>
2022-04-13 22:09:41 -0700

    lemonadep: overlay: Convert to RRO packages
    Change-Id: Iae6ef9fa9743bd2447991ad11d38fa184b12981d

--- device.mk
+++ device.mk
@@ -24,8 +24,12 @@
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
+
+PRODUCT_PACKAGES += \
+    OPlusFrameworksResTarget \
+    OPlusSettingsProviderResTarget \
+    OPlusSystemUIResTarget
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
@@ -42,6 +46,10 @@ PRODUCT_COPY_FILES += \
 # Device init scripts
 PRODUCT_PACKAGES += \
     init.recovery.target.rc
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
 
 PRODUCT_SHIPPING_API_LEVEL := 30
 
